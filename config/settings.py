0b64337d168ea85f315042dea5de2551feb5fa2b
yn_th <yasin.thmaseby13@gmail.com>
2026-06-27 23:28:00 +0330

    fix some bugs in settings

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY',default = 'fake-secret-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('ALLOWED_HOSTS',default = True , cast = bool)
+DEBUG = config('DEBUG',default = False , cast = bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',default = '127.0.0.1,localhost',cast = Csv())
 
